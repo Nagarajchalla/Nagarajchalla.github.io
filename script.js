d99383e564de20ecca5169e28e6cde963f1fa34e
Nagarajchalla <163152962+Nagarajchalla@users.noreply.github.com>
2026-01-17 22:08:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-const menuBtn = document.getElementById("menuBtn");
-const sideMenu = document.getElementById("sideMenu");
-
-menuBtn.addEventListener("click", () => {
-  sideMenu.style.display =
-    sideMenu.style.display === "block" ? "none" : "block";
-});
-
-document.querySelectorAll(".side-menu a").forEach(link => {
-  link.addEventListener("click", () => {
-    sideMenu.style.display = "none";
-  });
-});
+function toggleMenu() {
+  document.getElementById("sidebar").classList.toggle("active");
+}
